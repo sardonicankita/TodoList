2f55899a6b7e5d5d73b8ea4c0ae6638ebf14496c
sardonicankita <67410826+sardonicankita@users.noreply.github.com>
2022-06-06 03:25:45 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1><u>My Todo List</u></h1>
+        </div>
+    )
+}
+
+export default Header;
